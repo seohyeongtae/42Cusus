c97dc6b7f5050942503ca38b6b9a7a9ec7c6d422
hyseo <tmahek1@naver.com>
2022-04-20 20:19:32 +0900

    cpp05 ex00

--- CPP_Module/CPP_Module_05/ex00/Bureaucrat.cpp
+++ CPP_Module/CPP_Module_05/ex00/Bureaucrat.cpp
@@ -6,7 +6,81 @@
 /*   By: hyseo <hyseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 17:42:36 by hyseo             #+#    #+#             */
-/*   Updated: 2022/03/24 17:42:37 by hyseo            ###   ########.fr       */
+/*   Updated: 2022/04/20 20:12:36 by hyseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() { std::cout << "Bureaucrat Constructor Called" << std::endl; }
+
+Bureaucrat::Bureaucrat(const std::string &name, const int &grade) : name(name), grade(grade) {
+    if (grade < 0)
+        throw(GradeTooHighException());
+    else if (grade > 150)
+        throw(GradeTooLowException());
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &b) : name(b.getName()), grade(b.getGrade())
+{
+    if (grade < 0)
+        throw(GradeTooHighException());
+    else if (grade > 150)
+        throw(GradeTooLowException());
+}
+
+Bureaucrat  &Bureaucrat::operator=(const Bureaucrat &b)
+{
+    if (this != &b)
+    {
+        *(const_cast<std::string *>(&this->name)) = b.getName();
+        grade = b.getGrade();
+    }
+    return (*this);
+}
+
+const std::string  Bureaucrat::getName() const
+{
+    return name;
+}
+
+const int   Bureaucrat::getGrade() const
+{
+    return grade;
+}
+
+int&    Bureaucrat::incrementGrade()
+{
+    if (grade -= 1 < 0)
+    {
+        throw(GradeTooHighException());
+    } else
+        grade += 1;
+    return grade;
+}
+
+int&    Bureaucrat::decrementGrade()
+{
+    if (grade += 1 > 150)
+    {
+        throw(GradeTooLowException());
+    } else
+        grade -= 1;
+    return grade;
+}
+
+const char* Bureaucrat::GradeTooHighException::what(void) const throw()
+{
+    return ("GradeTooHighException");
+}
+
+const char* Bureaucrat::GradeTooLowException::what(void) const throw()
+{
+    return ("GradeTooLowException");
+}
+
+Bureaucrat::~Bureaucrat() { std::cout << "Bureaucrat Destructor Called" << std::endl; }
+
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& b) {
+  return o << "<" << b.getName() << ">, bureaucrat grade <" << b.getGrade() << ">";
+}
--- CPP_Module/CPP_Module_05/ex00/main.cpp
+++ CPP_Module/CPP_Module_05/ex00/main.cpp
@@ -6,7 +6,50 @@
 /*   By: hyseo <hyseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 17:42:45 by hyseo             #+#    #+#             */
-/*   Updated: 2022/03/24 17:42:45 by hyseo            ###   ########.fr       */
+/*   Updated: 2022/04/20 20:15:22 by hyseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+void b_call(Bureaucrat& b, int& (Bureaucrat::*f)(void)) {
+  try {
+    // (*(const_cast<Bureaucrat*>(&b)).*f)();
+    (b.*f)();
+    std::cout << b << std::endl;
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
+void b_make(const std::string& name, const int& grade) {
+  try {
+    Bureaucrat b(name, grade);
+    std::cout << b << std::endl;
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+}
+int main(void) {
+  Bureaucrat j("test", 1);
+  Bureaucrat h("test2", 150);
+
+  std::cout << j << std::endl
+    << h << std::endl;
+  std::cout << std::endl;
+  b_call(j, &Bureaucrat::incrementGrade);
+  b_call(h, &Bureaucrat::decrementGrade);
+  std::cout << std::endl;
+  b_make("test", 0);
+  b_make("test2", 151);
+  std::cout << std::endl;
+  b_make("test", 70);
+  b_make("test2", 70);
+  std::cout << std::endl;
+  b_call(j, &Bureaucrat::decrementGrade);
+  b_call(h, &Bureaucrat::incrementGrade);
+  std::cout << std::endl;
+  b_call(j, &Bureaucrat::decrementGrade);
+  b_call(h, &Bureaucrat::incrementGrade);
+  return 0;
+}
